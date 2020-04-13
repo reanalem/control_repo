d4b2e8c52c592e2963987d89996ba7ab6a847a46
Reanal Em <reanal.em@gmail.com>
2020-04-13 12:44:58 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -11,3 +11,6 @@ class profile::ssh_server {
                 user   => 'root',
                 type   => 'ssh-rsa',
                 key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDAo8y4oTWVxYj5yoj+TJQUUPiaLrAdrWXICY+/tDGOkIth0X9Ar6Stha1UjC3YgEbJUoRZKTPIqSbbLdINwD4jDzlL97H781C7FGa203yvoVvgy93QPD4IB4grnl2mEZp+cRq/AJu+6yw5593Mfv9pPS0nHKQKOlWAbsgm3cMX8DxfOFa58JQIwXQKI2oqxxTbNXyJJe+BdLFVJa7diev4tqkC9dU3FwPgUIOn6gS9ENlSpc5DuvRzXjk94An3D7JuuLu6fqL+dbREiQ+gB+O37UStPNHaXjhxiyOmrGzYZWWa+FFVwj3Lqs07xfArmy0AmgU0hS7o8JdIeCA9TXN1',
+         }
+ }
+   
